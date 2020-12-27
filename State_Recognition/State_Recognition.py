369c7ac1c1b22941cbba5c41b08632f511dd1ead
tobi <tobi@tobias-huebner.org>
2020-12-27 22:36:27 +0100

    loop

--- State_Recognition/State_Recognition.py
+++ State_Recognition/State_Recognition.py
@@ -1,9 +1,9 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-import pyautogui
 from mss import mss
 import os
+import time
 
 class Weapon:
     def __init__(self,name, kp, des, img):
@@ -15,7 +15,7 @@ class Weapon:
 
 meth ='cv2.TM_CCOEFF_NORMED'
 weapons_dir = "weapons1080p";
-threshold = 0.3
+threshold = 0.5
 
 sift = cv2.SIFT_create()
 bf = cv2.BFMatcher()
@@ -34,36 +34,47 @@ with mss() as sct:
     # 0 monitor is all monitors - 1 is primary
     monitor = sct.monitors[1] 
    
-    screenshot = cv2.cvtColor(np.array(sct.grab(monitor)), cv2.COLOR_RGB2GRAY)
 
-    kp, des = sift.detectAndCompute(screenshot,None)
+    while True:
+        screenshot = cv2.cvtColor(np.array(sct.grab(monitor)), cv2.COLOR_RGB2GRAY)
 
-    results = []
-    # iterate all weapons and show matches
-    for weapon in weapons:
-        wc = weapons[weapon]
-        # first comes the big picture, the soruce, then what you want to find in it
-        matches = bf.knnMatch(des,wc.des,k=2)
-        # Apply ratio test
-        good = []
-        for m,n in matches:
-            if m.distance < threshold * n.distance:
-                good.append(m)
+        kp, des = sift.detectAndCompute(screenshot,None)
+
+        results = []
+        # iterate all weapons and show matches
+        for weapon in weapons:
+            wc = weapons[weapon]
+            # first comes the big picture, the soruce, then what you want to find in it
+            matches = bf.knnMatch(des,wc.des,k=2)
+            # Apply ratio test
+            good = []
+            for m,n in matches:
+                if m.distance < threshold * n.distance:
+                    good.append(m)
 
 
-        results.append((wc, len(good), good));
+            results.append((wc, len(good), good));
  
 
-    winner = sorted(results, key=lambda tup: tup[1])[-1];
-    
-    # => dont seem to need pixel color selection, because key points are different in selected and non selected weapons
-    goods = winner[2]
-    winner_wc = winner[0]
+        res_sorted = sorted(results, key=lambda tup: tup[1])
 
-    for good in goods:
+        for res in res_sorted:
+            print(res[0].name + " " + str(res[1]))
+        winner = res_sorted[-1]
+    
+
+
+        # => dont seem to need pixel color selection, because key points are different in selected and non selected weapons
+        goods = winner[2]
+        winner_wc = winner[0]
+
+        for good in goods:
         
-        kp = winner_wc.kp[good.trainIdx]
+            kp = winner_wc.kp[good.trainIdx]
     
-        px_color = winner_wc.img[round(kp.pt[1]), round(kp.pt[0])]
+            px_color = winner_wc.img[round(kp.pt[1]), round(kp.pt[0])]
 
-        print(px_color)
+            print(px_color)
+
+        time.sleep(5)
+        print()
